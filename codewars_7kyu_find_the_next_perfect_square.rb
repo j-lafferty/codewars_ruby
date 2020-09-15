97e58c358fdac0d95b95ca6fa3af5bf9893cb861
James Lafferty <james@j-lafferty.net>
2020-09-15 11:29:27 -0700

    Initial commit

---
+++ codewars_7kyu_find_the_next_perfect_square.rb
@@ -0,0 +1,19 @@
+# You might know some pretty large perfect squares. But what about the NEXT one?
+
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+# If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+
+def find_next_square(sq)
+  # Return the next square if sq is a perfect square, -1 otherwise
+  -1
+end
+
+# Examples:
+
+puts find_next_square(121) == 144
+puts find_next_square(625) == 676
+puts find_next_square(319225) == 320356
+puts find_next_square(15241383936) == 15241630849
+puts find_next_square(155) == -1
+puts find_next_square(342786627) == -1
