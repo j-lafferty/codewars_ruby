1962def13a8d441245a71894098cbb7e957fea21
James Lafferty <james@j-lafferty.net>
2020-09-15 12:53:37 -0700

    Add solution

--- codewars_6kyu_format_string_of_names.rb
+++ codewars_6kyu_format_string_of_names.rb
@@ -3,7 +3,17 @@
 # Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
 
 def list(names)
+  formatted = names.map do |i|
+    if names.index(i) < names.size - 2
+      i[:name] + ','
+    elsif names.index(i) == names.size - 1 && names.size > 1
+      '& ' + i[:name]
+    else
+      i[:name]
+    end
+  end
 
+  formatted.join(' ')
 end
 
 # Example:
